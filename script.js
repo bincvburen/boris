1a3f064c12f746d500ea2f136348cbcbf6659625
Binc <bincvanburen@icloud.com>
2024-04-25 00:25:02 +0200

    niceee final

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle('show-menu');
   menu.classList.toggle('show');
 }
+
+
+function checkScreenSize() {
+  if (window.matchMedia("(min-width: 600px)").matches) {
+      // Als de schermbreedte kleiner is dan of gelijk is aan 600px
+      menu.classList.remove('show-menu');
+      menu.classList.remove('show');
+  }}
+
+  window.addEventListener('resize', checkScreenSize);
